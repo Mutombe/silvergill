0368a457112c23facb264a4c2f4f3bd97bc99e98
Mutombe <simbarashemutombe1@gmail.com>
2026-01-13 22:47:33 +0200

    scroll

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import Layout from './components/layout/Layout';
 
@@ -12,6 +12,16 @@ import Media from './pages/Media';
 import Gallery from './pages/Gallery';
 import Careers from './pages/Careers';
 import Contact from './pages/Contact';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   return (
@@ -27,6 +37,7 @@ function App() {
           },
         }}
       />
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
